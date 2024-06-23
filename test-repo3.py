fc9acf519c6af98aa3ae68c59770e2b9fc99baf9
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-06-22 16:19:09 -0800

    Create test-repo3.py

---
+++ test-repo3.py
@@ -0,0 +1,9 @@
+# Multiple two integers
+first = int(input("Enter your first num: "))
+second = int(input("Enter your second num: "))
+
+# To multiple two integers
+multiple = first * second 
+
+# To print the result
+print("The result of: " + str(multiple))
